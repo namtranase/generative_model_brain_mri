b9c32036d494935619496da6b6795f773cc99427
tranducnamASE <trannam.ase@gmail.com>
2020-11-09 16:24:38 +0700

    update: def load data for classify program

--- src/classification/classification.py
+++ src/classification/classification.py
@@ -56,6 +56,29 @@ def split_train_test_val():
 
     return True
 
+def load_data(dir_path, img_size=(100, 100)):
+    """Load and resize images.
+    """
+    X = []
+    y = []
+    i = 0
+    labels = dict()
+    for path in tqdm(sorted(os.listdir(dir_path))):
+        if not path.startswith('.'):
+            labels[i] = path
+            for file in os.listdir(dir_path + path):
+                if not file.startswith('.'):
+                    img = cv2.imread(dir_path + path + '/' + file)
+                    X.append(img)
+                    y.append(i)
+            i += 1
+
+    X = np.array(X)
+    y = np.array(y)
+    print(f'{len(X)} images loaded from {dir_path} directory.')
+
+    return X, y, labels
+
 def main():
     # Train, test, val split
     status_split = split_train_test_val()
