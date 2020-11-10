5deec9866d626a13d1a54027edec8800908e99d9
tranducnamASE <trannam.ase@gmail.com>
2020-11-10 15:15:06 +0700

    update: clean split function

--- src/classification/classification.py
+++ src/classification/classification.py
@@ -1,17 +1,23 @@
-import logging
+"""
+Copyright 2020 ASE Laboratory.
+@author namtran.ase
 
-import numpy as np
-from tqdm import tqdm
-import cv2
+Implement python class for classify mri brain images.
+"""
 import os
-import shutil
 import itertools
+import logging
+from tqdm import tqdm
+
+import shutil
 import imutils
+import cv2
+import numpy as np
+
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
-
 import plotly.graph_objs as go
 from plotly.offline import init_notebook_mode, iplot
 from plotly import tools
@@ -28,35 +34,35 @@ from src.config.config import read_config_file
 
 RANDOM_SEED = 123
 
-IMG_PATH = 'data/kaggle_mri_classification/brain_tumor_dataset/'
-SPLIT_PATH = 'data/kaggle_mri_classification/'
-TRAIN_DIR = 'data/kaggle_mri_classification/TRAIN/'
-TEST_DIR = 'data/kaggle_mri_classification/TEST/'
-VAL_DIR = 'data/kaggle_mri_classification/VAL/'
+def split_train_test_val(config):
+    """Split dataset to train, test and validation set.
+    """
+    img_path = config['classification']['vgg16']['img_path']
+    split_path = config['classification']['vgg16']['split_path']
 
-def split_train_test_val():
-    if not os.path.exists(SPLIT_PATH + 'TRAIN'):
-            os.makedirs(SPLIT_PATH + 'TRAIN/YES')
-            os.makedirs(SPLIT_PATH + 'TRAIN/NO')
-            os.makedirs(SPLIT_PATH + 'TEST/YES')
-            os.makedirs(SPLIT_PATH + 'TEST/NO')
-            os.makedirs(SPLIT_PATH + 'VAL/YES')
-            os.makedirs(SPLIT_PATH + 'VAL/NO')
+    if not os.path.exists(split_path + 'train'):
+            os.makedirs(split_path + 'train/yes')
+            os.makedirs(split_path + 'train/no')
+            os.makedirs(split_path + 'test/yes')
+            os.makedirs(split_path + 'test/no')
+            os.makedirs(split_path + 'val/yes')
+            os.makedirs(split_path + 'val/no')
 
-    for CLASS in os.listdir(IMG_PATH):
-        if CLASS.startswith('.'):
+    for _class in os.listdir(img_path):
+        if _class.startswith('.'):
             return False
-        IMG_NUM = len(os.listdir(IMG_PATH + CLASS))
-        for (n, FILE_NAME) in enumerate(os.listdir(IMG_PATH + CLASS)):
-            img = IMG_PATH + CLASS + '/' + FILE_NAME
+        img_num = len(os.listdir(img_path + _class))
+        for (n, FILE_NAME) in enumerate(os.listdir(img_path + _class)):
+            img = img_path + _class + '/' + FILE_NAME
+
             if n < 5:
-                des_test = SPLIT_PATH + 'TEST/' + CLASS.upper() + '/'
+                des_test = split_path + 'test/' + _class + '/'
                 shutil.copy(img, des_test)
-            elif n < 0.8*IMG_NUM:
-                des_train = SPLIT_PATH + 'TRAIN/' + CLASS.upper() + '/'
+            elif n < 0.8*img_num:
+                des_train = split_path + 'train/' + _class + '/'
                 shutil.copy(img, des_train)
             else:
-                des_val = SPLIT_PATH + 'VAL/' + CLASS.upper() + '/'
+                des_val = split_path + 'val/' + _class + '/'
                 shutil.copy(img, des_val)
 
     return True
@@ -95,16 +101,17 @@ def main():
     """Main program
     """
     config = read_config_file(settings.config_file)
-    print(config)
-    raise 1==2
     if config['debug']:
         logging.basicConfig(level=logging.INFO)
-    config = read_config_file(setting.config_file)
 
-    # Train, test, val split
-    status_split = split_train_test_val()
+    # train, test, val split
+    status_split = split_train_test_val(config)
     logging.info("Status of split oeration: %s",status_split)
 
+    # Build data to training
+    train_dir = 'data/kaggle_mri_classification/train/'
+    test_dir = 'data/kaggle_mri_classification/test/'
+    val_dir = 'data/kaggle_mri_classification/val/'
     logging.info("Program was terminated!")
 
 if __name__ == "__main__":
